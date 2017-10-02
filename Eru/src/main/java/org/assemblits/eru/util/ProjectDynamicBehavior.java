e5ee651d159aee2da10ecf6c96fa6778069bd27e
marlontrujillo1080 <marlontrujillo1080@gmail.com>
2017-10-02 18:10:09 -0400

    Tag revised. Fix #67 #69 #70

--- Eru/src/main/java/org/assemblits/eru/util/ProjectDynamicBehavior.java
+++ Eru/src/main/java/org/assemblits/eru/util/ProjectDynamicBehavior.java
@@ -41,7 +41,6 @@ public class ProjectDynamicBehavior {
 
     public void setProjectModel(ProjectModel projectModel) {
         this.projectModel = projectModel;
-
         addListeners();
     }
 
@@ -138,37 +137,19 @@ public class ProjectDynamicBehavior {
         TagLink link;
         switch (tag.getType()) {
             case INPUT:
+                if (tag.getLinkedAddress() == null) return;
                 link = new AddressChangeLink(tag);
                 tag.getLinkedAddress().timestampProperty().addListener(link);
                 registerLink(tag, link);
                 break;
-            case MASK:
-                link = new TagCurrentValueLinkForMaskUpdating(tag.getLinkedTag(), tag);
-                tag.getLinkedTag().valueProperty().addListener(link);
-                registerLink(tag.getLinkedTag(), link);
-                break;
             case MATH:
-                projectModel.getTags().stream().filter(t -> tag.getScript().contains(t.getName())).forEach(tagInScript -> {            // Find tags in the script
-                    TagLink scriptUpdating = new TagCurrentValueLinkForScriptUpdating(tagInScript, tag);
-                    tagInScript.valueProperty().addListener(scriptUpdating);
-                    registerLink(tagInScript, scriptUpdating);
-                });
-                break;
-            case STATUS:
-                Map<String, String> statusMap = new HashMap<>();                                                      // Create Map value:statusName
-                StringTokenizer scriptTokenizer = new StringTokenizer(tag.getScript() == null ? "" : tag.getScript(), ",");
-                while (scriptTokenizer.hasMoreElements()) {
-                    String pair = scriptTokenizer.nextElement().toString();
-                    StringTokenizer pairTokenizer = new StringTokenizer(pair, "=");
-                    while (pairTokenizer.hasMoreElements()) {
-                        String value = pairTokenizer.nextElement().toString();
-                        String status = pairTokenizer.nextElement().toString();
-                        statusMap.put(value, status);
-                    }
-                }
-                link = new TagCurrentValueLinkForStatusUpdating(tag.getLinkedTag(), tag, statusMap);
-                tag.getLinkedTag().valueProperty().addListener(link);
-                registerLink(tag.getLinkedTag(), link);
+                projectModel.getTags().stream()
+                        .filter(t -> tag.getScript().contains(t.getName()))
+                        .forEach(tagInScript -> {            // Find tags in the script
+                            TagLink scriptUpdating = new TagCurrentValueLinkForScriptUpdating(tagInScript, tag);
+                            tagInScript.valueProperty().addListener(scriptUpdating);
+                            registerLink(tagInScript, scriptUpdating);
+                        });
                 break;
             case OUTPUT:
                 break;
@@ -259,18 +240,6 @@ class AddressChangeLink extends TagLink {
     }
 }
 
-class TagCurrentValueLinkForMaskUpdating extends TagLink {
-    TagCurrentValueLinkForMaskUpdating(Tag tagToListen, Tag tagToUpdate) {
-        super(tagToListen, tagToUpdate);
-    }
-
-    @Override
-    protected void updateValueAndTimestamp() throws Exception {
-        tagToUpdate.setValue(String.valueOf((Integer.getInteger(tagToListen.getValue())) & tagToUpdate.getMask()));
-        tagToUpdate.setTimestamp(tagToListen.getTimestamp());
-    }
-}
-
 class TagCurrentValueLinkForScriptUpdating extends TagLink {
     TagCurrentValueLinkForScriptUpdating(Tag tagToListen, Tag tagToUpdate) {
         super(tagToListen, tagToUpdate);
@@ -282,20 +251,5 @@ class TagCurrentValueLinkForScriptUpdating extends TagLink {
             throw new TagLinkException(tagToUpdate + "has a null script");
         tagToUpdate.setValue(String.valueOf(EngineScriptUtil.getInstance().getScriptEngine().eval(tagToUpdate.getScript())));
         tagToUpdate.setTimestamp(tagToListen.getTimestamp() == null ? new Timestamp(System.currentTimeMillis()) : tagToListen.getTimestamp());
-    }
-}
-
-class TagCurrentValueLinkForStatusUpdating extends TagLink {
-    private final Map<String, String> statusMap;
-
-    TagCurrentValueLinkForStatusUpdating(Tag tagToListen, Tag tagToUpdate, Map<String, String> statusMap) {
-        super(tagToListen, tagToUpdate);
-        this.statusMap = statusMap;
-    }
-
-    @Override
-    protected void updateValueAndTimestamp() throws Exception {
-        tagToUpdate.setValue(statusMap.get(tagToListen.getValue()));
-        tagToUpdate.setTimestamp(tagToListen.getTimestamp());
     }
 }
