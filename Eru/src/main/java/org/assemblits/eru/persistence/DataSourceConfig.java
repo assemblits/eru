a17ff60e5002549f0230f3c38781c072de696935
marlontrujillo1080 <marlontrujillo1080@gmail.com>
2017-10-29 13:18:32 -0400

    H2 database is a backup. Fix #96

--- Eru/src/main/java/org/assemblits/eru/persistence/DataSourceConfig.java
+++ Eru/src/main/java/org/assemblits/eru/persistence/DataSourceConfig.java
@@ -19,9 +19,9 @@
 package org.assemblits.eru.persistence;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assemblits.eru.preferences.EruPreferences;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -35,13 +35,42 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 @EnableJpaRepositories("org.assemblits.eru")
 @EntityScan("org.assemblits.eru")
-public class MainDataSourceConfig {
 
+public class DataSourceConfig {
+
+    private static final String USERNAME = "eru";
+    private static final String PASSWORD = "951753";
+
+    @Bean
     @Primary
-    @Bean(name = "mainDataSource")
-    @ConfigurationProperties(prefix = "main.datasource")
-    public DataSource mainDataSource() {
-        return DataSourceBuilder.create().build();
+    public DataSource dataSource() {
+        DataSource dataSource;
+        try {
+            dataSource = getMainDataSource();
+            dataSource.getConnection().isValid(500);
+        } catch (Exception e) {
+            log.error("Main database not valid.", e);
+            dataSource =getBackupDataSource();
+        }
+        return dataSource;
     }
 
+    private DataSource getMainDataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.jdbc.Driver")
+                .username(USERNAME)
+                .password(PASSWORD)
+                .url("jdbc:mysql://localhost:3306/eru?useSSL=false")
+                .build();
+    }
+
+    private DataSource getBackupDataSource() {
+        final EruPreferences eruPreferences = new EruPreferences();
+        return DataSourceBuilder.create()
+                .driverClassName("org.h2.Driver")
+                .username(USERNAME)
+                .password(PASSWORD)
+                .url("jdbc:h2:" + eruPreferences.getApplicationDirectory().get() + "/eru;DB_CLOSE_ON_EXIT=FALSE")
+                .build();
+    }
 }
